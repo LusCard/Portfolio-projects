f54a95bc949f4af356f786b69831bdde66519240
LusCard <luquiscardenas@gmail.com>
2025-12-11 17:15:15 -0300

    Revert to default config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/Portfolio-projects",
+  /* config options here */
 };
 
 export default nextConfig;
